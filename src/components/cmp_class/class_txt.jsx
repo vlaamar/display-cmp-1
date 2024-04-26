09ca4b8020ebb2c1bdaea562cbe31244835a988f
Vlasta M <vmarinovic@gmail.com>
2024-04-26 17:20:13 +0200

    prvi zadatak, prvi commit

---
+++ src/components/cmp_class/class_txt.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+export default class displayTxtClass extends React.Component {
+  render() {
+    return (
+      <>
+        <h3>
+          Zadatak 1.3 Napraviti jednu komponentu definiranu
+          klasom koja vraća JSX s tekstom
+        </h3>
+        <p>
+          Evo teksta: Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Labore illum eaque ab sit cupiditate animi voluptatum magnam, unde,
+          tempore assumenda repellendus odio nam adipisci nihil doloribus,
+          eligendi iure tenetur esse!
+        </p>
+      </>
+    );
+  }
+}
---
+++ src/components/cmp_func/func_txt.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function displayText() {
+  return (
+    <>
+      <h3>
+        Zadatak 1.2 Napraviti jednu komponentu definiranu
+        funkcijom koja vraća JSX s tekstom
+      </h3>
+      <p>
+        Evo teksta: Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Labore illum eaque ab sit cupiditate animi voluptatum magnam, unde,
+        tempore assumenda repellendus odio nam adipisci nihil doloribus,
+        eligendi iure tenetur esse!
+      </p>
+    </>
+  );
+}
